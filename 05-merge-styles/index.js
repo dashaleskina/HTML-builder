68ee5bd8737ef4f3ebc2e89ae78fae3defeffa3b
Daria Leskina <dleskinaleskina@yandex.ru>
2025-01-20 16:10:43 +0300

    refactor: change logic in 05-merge-styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,40 +1,41 @@
 const fs = require('fs');
 const path = require('path');
 
-// директория для бандла
 const projectDist = path.join(__dirname, 'project-dist', 'bundle.css');
-//директория со стилями
 const stylesFolder = path.join(__dirname, 'styles');
-//создание файла
-const writeStream = fs.createWriteStream(projectDist);
 
 const createBundle = () => {
-  fs.readdir(stylesFolder, (err, files) => {
-    if (err) {
-      return console.error('Error reading directory:', err);
+  fs.unlink(projectDist, (err) => {
+    if (err && err.code !== 'ENOENT') {
+      return console.error('Error deleting', err);
     }
 
-    files.forEach((item) => {
-      const fullPath = path.join(stylesFolder, item);
+    fs.readdir(stylesFolder, (err, files) => {
+      if (err) {
+        return console.error('Error reading directory:', err);
+      }
 
-      // Проверка, является ли элемент файлом и имеет ли он расширение .css
-      fs.stat(fullPath, (err, stats) => {
-        if (err) {
-          return console.error('Error getting file stats:', err);
-        }
+      files.forEach((item) => {
+        const fullPath = path.join(stylesFolder, item);
 
-        if (stats.isFile() && item.endsWith('.css')) {
-          fs.readFile(fullPath, (err, info) => {
-            if (err) {
-              return console.error('Error reading file:', err);
-            }
-            fs.appendFile(projectDist, info.toString(), (writeError) => {
-              if (writeError) {
-                console.error('Error when adding data:', writeError);
+        fs.stat(fullPath, (err, stats) => {
+          if (err) {
+            return console.error('Error getting file stats:', err);
+          }
+
+          if (stats.isFile() && item.endsWith('.css')) {
+            fs.readFile(fullPath, (err, info) => {
+              if (err) {
+                return console.error('Error reading file:', err);
               }
+              fs.appendFile(projectDist, info.toString(), (writeError) => {
+                if (writeError) {
+                  console.error('Error when adding data:', writeError);
+                }
+              });
             });
-          });
-        }
+          }
+        });
       });
     });
   });
